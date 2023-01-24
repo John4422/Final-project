73fb1885243faf6278a74eca137831f6210d66d5
John Steinburg <Johnstein456@hotmail.com>
2023-01-23 19:45:49 -0500

    continue at school

--- Game1.cs
+++ Game1.cs
@@ -3,6 +3,7 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
+using System.Drawing;
 
 namespace Final_project
 {
@@ -17,11 +18,13 @@ namespace Final_project
         Texture2D right;
         Texture2D astaroid;
         Texture2D space;
+        Texture2D end;
         Rectangle standard;
         Rectangle asteroid;
         Rectangle asteroid2;
         Rectangle asteroid3;
         Rectangle Space;
+        Rectangle endRect;
         Vector2 astaroidspeed;
         Vector2 speed;
         KeyboardState keyboardState;
@@ -31,7 +34,7 @@ namespace Final_project
         {
             intro,
             ship,
-
+            end,
         }
 
         public Game1()
@@ -59,12 +62,14 @@ namespace Final_project
             astaroidspeed = new Vector2(0, 0);
             speed = new Vector2(0, 0);
             Space = new Rectangle(0, 0, _graphics.PreferredBackBufferWidth, _graphics.PreferredBackBufferHeight);
+            endRect = new Rectangle(0, 0, _graphics.PreferredBackBufferWidth, _graphics.PreferredBackBufferHeight);
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
             space = Content.Load<Texture2D>("space");
+            end = Content.Load<Texture2D>("End Theme");
             standerd = Content.Load<Texture2D>("standerd");
             left = Content.Load<Texture2D>("fast-left");
             right = Content.Load<Texture2D>("fast-right");
@@ -112,7 +117,45 @@ namespace Final_project
 
             standard.X += (int)speed.X;
             standard.Y += (int)speed.Y;
-            }
+
+                if (screen == Screen.ship)
+                {
+                    if (standard.Intersects(asteroid) || standard.Intersects(asteroid2) || standard.Intersects(asteroid3))
+                    {
+                       screen = Screen.end
+                    }
+                }
+                asteroid.X += (int)astaroidspeed.X;
+                asteroid.Y += (int)astaroidspeed.Y;
+                asteroid2.X += (int)astaroidspeed.X;
+                asteroid2.Y += (int)astaroidspeed.Y;
+                asteroid3.X += (int)astaroidspeed.X;
+                asteroid3.Y += (int)astaroidspeed.Y;
+
+                if (asteroid.X < 0 || asteroid.X > _graphics.PreferredBackBufferWidth - asteroid.Width)
+                {
+                    astaroidspeed.X = -astaroidspeed.X;
+                }
+                if (asteroid.Y < 0 || asteroid.Y > _graphics.PreferredBackBufferHeight - asteroid.Height)
+                {
+                    astaroidspeed.Y = -astaroidspeed.Y;
+                }
+                if (asteroid2.X < 0 || asteroid2.X > _graphics.PreferredBackBufferWidth - asteroid2.Width)
+                {
+                    astaroidspeed.X = -astaroidspeed.X;
+                }
+                if (asteroid2.Y < 0 || asteroid2.Y > _graphics.PreferredBackBufferHeight - asteroid2.Height)
+                {
+                    astaroidspeed.Y = -astaroidspeed.Y;
+                }
+                if (asteroid3.X < 0 || asteroid3.X > _graphics.PreferredBackBufferWidth - asteroid3.Width)
+                {
+                    astaroidspeed.X = -astaroidspeed.X;
+                }
+                if (asteroid3.Y < 0 || asteroid3.Y > _graphics.PreferredBackBufferHeight - asteroid3.Height)
+                {
+                    astaroidspeed.Y = -astaroidspeed.Y;
+                }
            
 
             // TODO: Add your update logic here
@@ -124,6 +167,9 @@ namespace Final_project
         {
             GraphicsDevice.Clear(Color.White);
             _spriteBatch.Begin();
+
+            if (screen == Screen.ship)
+            {
             _spriteBatch.Draw(space, Space, Color.White);
             if (speed.X > 0)
             {
@@ -141,6 +187,13 @@ namespace Final_project
             _spriteBatch.Draw(astaroid,asteroid,Color.White);
             _spriteBatch.Draw(astaroid, asteroid2, Color.White);
             _spriteBatch.Draw(astaroid, asteroid3, Color.White);
+            }
+            else if (screen == Screen.end)
+            {
+                _spriteBatch.Draw(end, endRect, Color.White);
+            }
+
+
             _spriteBatch.End();
             // TODO: Add your drawing code here
 
